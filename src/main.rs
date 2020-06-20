ca4eb121d4d2e173cd37119cc90a67a30d0845f5
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-06-20 00:15:44 -0300

    Save amount of published data.

--- src/main.rs
+++ src/main.rs
@@ -18,25 +18,39 @@ use std::io::prelude::*;
 extern crate env_logger;
 use log::{info, error};
 
+use ctrlc::set_handler;
+
 mod model;
+
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+fn handle_close() {
+  // info!("Published total values {}");
+}
 
 #[tokio::main]
 async fn main() {
   env_logger::init();
+
+  set_handler(move || handle_close()).expect("Error setting Ctrl-C handler");
 
   let address = env::var("MQTT_BROKER_ADDRESS").unwrap();
   let port = env::var("MQTT_BROKER_PORT").unwrap().parse::<u16>().unwrap();
 
   info!("Starting client. Host at {}:{}", address.clone(), port.clone());
 
+  let published: Arc<Mutex<HashMap<String, i64>>> = Arc::new(Mutex::new(HashMap::new()));
   let count = get_lines();
   
-  let (mut requests_tx, requests_rx) = channel(50);
+  let (requests_tx, requests_rx) = channel(50);
   let mut eloop;
   let mut mqttoptions = MqttOptions::new("client", address.clone(), port);
   mqttoptions.set_keep_alive(5).set_throttle(Duration::from_secs(1));
   eloop = eventloop(mqttoptions, requests_rx);
-
 
   let tx_c = requests_tx.clone();
   for i in 0..count {
@@ -44,25 +58,38 @@ async fn main() {
     let coordinates = assort_coordinates(i);
     
     let tx = tx_c.clone();
+    let published_cl = Arc::clone(&published);
     task::spawn(async move {
       info!("Thread {}", topic.clone());
-
+      let p = Arc::clone(&published_cl);
+      
       let api_address = format!(
         "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}", 
         coordinates.lat.to_string(),
         coordinates.lon.to_string(),
         "f02ef3d664e7ad0b33020a0ecbf896b2"
       );
-
+      
       let mut weather: model::Weather;
       loop {
         weather = reqwest::get(&api_address.clone()).await.unwrap().json().await.unwrap();
         let payload = (weather.condition.temp - 273.0).to_string();
         
-        for _i in 0..5 {
-          tx.clone().send(publish_request(&payload, &topic.clone())).await.unwrap();
-          info!("{}:{}", payload, topic.clone());
-          time::delay_for(Duration::from_millis(100)).await;
+        let t = topic.clone();
+        for _i in 0..15 {
+          tx.clone().send(publish_request(&payload, &t.clone())).await.unwrap();
+          
+          {
+            // Access global mutexed variable
+            let mut guard = p.lock().unwrap();
+            guard.entry(t.clone()).or_insert(0);
+            guard.insert(t.clone(), 1 );
+            drop(guard);
+          }
+
+          info!("{}:{}", payload, t);
+          
+          time::delay_for(Duration::from_millis(50)).await;
         }
       }
     });
